7779f8f07386f2d0e19d3bf50f22a78002f34b62
Amankumar10 <amankumar02740@gmail.com>
2021-07-23 21:15:03 +0530

    file added

--- js/GameView.js
+++ js/GameView.js
@@ -1,10 +1,16 @@
-export default class GameView { 
+export default class GameView {
 
-    constructor(){
-
+    constructor() {
+        console.log("init game view");
     }
-
-    updateBoard(game){
-        console.log(game.board)
+    updateBoard(game) {
+        console.log("this of a board within GameView")
+        console.log(game.board);
+        for(let i = 0;i<game.board.lenght; i++){
+            const tile = document.querySelector(`.board-tile[data-index="${i}"]`);
+            tile.textContent =game.board[i];
+        }
     }
 }
+
+
--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-import Game from "./Game"
-import GameView from './GameView'
+import Game from "./Game.js"
+import GameView from "./GameView.js"
 
-let Game = new Game();
-let GameView = new GameView();
+let game = new Game();
+let gameView = new GameView();
 
-console.log("hello");
+gameView.updateBoard(game);
 
-console.log("My turn", game.turn);
+console.log(game.board)
+console.log("My turn is" ,game.turn);
+game.nextTurn();
+console.log("My turn is" ,game.turn);
+game.makeMove(3);
+console.log(game.board);
+game.nextTurn();
+game.makeMove(8);
+console.log(game.board)
+gameView.updateBoard(game);
 
-game.nextTurn()
